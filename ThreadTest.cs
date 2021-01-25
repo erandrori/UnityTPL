45742625d28328e3699b805ad2fab7b56180fa6a
Eran Drori <technolp@gmail.com>
2021-01-25 22:20:05 +0200

    Update ThreadTest.cs

--- ThreadTest.cs
+++ ThreadTest.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,19 +6,15 @@ public class ThreadTest : MonoBehaviour
 {
     void Start()
     {
-        var options = new ParallelOptions();
-
         var values = new int[100000000];
 
-        Parallel.For(0, values.Length, options, i =>
+        Parallel.For(0, values.Length, i =>
       {
           values[i] = i;
 
           if (i % 1000000 == 0)
               Debug.Log("Thread-" + Thread.CurrentThread.ManagedThreadId + " worked hard! Currently i is " + i.ToString());
-
-          i++;
-      });
+      }); // Parallel.For
 
         Debug.Log(values[50000001]);
     }

727589593144e4b23fcd196cb608e20d634041fd
Eran Drori <technolp@gmail.com>
2021-01-25 22:06:07 +0200

    Add files via upload

---
+++ ThreadTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class ThreadTest : MonoBehaviour
+{
+    void Start()
+    {
+        var options = new ParallelOptions();
+
+        var values = new int[100000000];
+
+        Parallel.For(0, values.Length, options, i =>
+      {
+          values[i] = i;
+
+          if (i % 1000000 == 0)
+              Debug.Log("Thread-" + Thread.CurrentThread.ManagedThreadId + " worked hard! Currently i is " + i.ToString());
+
+          i++;
+      });
+
+        Debug.Log(values[50000001]);
+    }
+}
